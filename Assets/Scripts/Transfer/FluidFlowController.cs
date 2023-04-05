43db16fc70e57d6081974a8825576d90a3166576
ShreyanshDarshan <shreyanshdarshan1@gmail.com>
2023-04-05 08:44:23 +0530

    Add particle effects for fluid overflow

---
+++ Assets/Scripts/Transfer/FluidFlowController.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FluidFlowController : MonoBehaviour
+{
+    [SerializeField] private int numEmitters = 10;
+    [SerializeField] private float radius = 0.01f;
+    [SerializeField] private Transform surface;
+    [SerializeField] private GameObject thinCapEmitter;
+    [SerializeField] private GameObject wideCapEmitter;
+    Transform[] particleEmitters;
+    public float flowRateMultiplier = 300.0f;
+    public float startSpeedMultiplier = 30.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        particleEmitters = GetComponentsInChildren<Transform>();
+        radius = GetComponent<SphereCollider>().radius;
+        GameObject wideCapInstantiated = Instantiate(wideCapEmitter, transform.position, transform.rotation);
+        wideCapInstantiated.transform.parent = transform;
+        wideCapInstantiated.GetComponent<OverflowDetector>().surface = surface;
+        for (int i = 0; i < numEmitters; i++)
+        {
+            float angle = i * Mathf.PI * 2 / numEmitters;
+            Vector3 pos = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0) * radius;
+            GameObject thinCapInstantiated = Instantiate(thinCapEmitter, transform.position + transform.TransformDirection(pos), transform.rotation);
+            thinCapInstantiated.transform.parent = transform;
+            thinCapInstantiated.GetComponent<OverflowDetector>().surface = surface;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (Transform emitter in transform)
+        {
+            GameObject emitterObject = emitter.gameObject;
+            OverflowDetector overflowDetector = emitterObject.GetComponent<OverflowDetector>();
+            // if (overflowDetector.isOverflowing)
+            // {
+
+            ParticleSystem ps = emitterObject.GetComponent<ParticleSystem>();
+            var emission = ps.emission;
+            emission.rateOverTimeMultiplier = Mathf.Clamp(overflowDetector.distanceFromSurface * flowRateMultiplier, 0.0f, 30.0f);
+            var main = ps.main;
+            main.startSpeedMultiplier = Mathf.Clamp(overflowDetector.distanceFromSurface * startSpeedMultiplier, 0.0f, 30.0f);
+            Debug.Log("Emission rate: " + emission.rateOverTimeMultiplier);
+            Debug.Log("Start speed: " + main.startSpeedMultiplier);
+
+            // ps.emission = emission;
+            // ps.Play();
+
+            // }
+            // else
+            // {
+            //     ParticleSystem ps = emitterObject.GetComponent<ParticleSystem>();
+            //     var emission = ps.emission;
+            //     emission.rateOverTime = 0.0f;
+            //     // ps.Pause();
+            // }
+        }
+    }
+}
---
+++ Assets/Scripts/Transfer/OverflowDetector.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverflowDetector : MonoBehaviour
+{
+    public Transform surface;
+    public float distanceFromSurface;
+    // public bool isOverflowing;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // isOverflowing = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Plane plane = new Plane(surface.forward, surface.position);
+        distanceFromSurface = plane.GetDistanceToPoint(transform.position);
+        // distanceFromSurface 
+        // if (distance > 0)
+        // {
+        //     isOverflowing = true;
+        // }
+        // else
+        // {
+        //     isOverflowing = false;
+        // }
+    }
+}
